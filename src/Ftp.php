36c100253db1a7dc0468079cf33522eb988318ec
josecarlosphp <38591375+josecarlosphp@users.noreply.github.com>
2020-11-01 13:38:45 +0100

    Corregir comentario de cabecera.

--- src/Ftp.php
+++ src/Ftp.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of josecarlosphp/reader - PHP classes to read from different sources.
+ * This file is part of josecarlosphp/internet - PHP classes to read from different sources.
  *
  * josecarlosphp/internet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
